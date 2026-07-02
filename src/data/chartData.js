a2075f3fd9ad63cdbc284d11cb1fbdd35cd904f6
Uswatun Hasanah <uswatunhasanahu297@gmail.com>
2026-07-02 23:28:41 +0700

    feat: menambahkam chartdata

---
+++ src/data/chartData.js
@@ -0,0 +1,6 @@
+export const chartDistribution = [
+  { name: "MRT Jakarta", percentage: "45%", color: "bg-[#7c4dff]" },
+  { name: "KRL Commuter", percentage: "24%", color: "bg-[#22c55e]" },
+  { name: "TransJakarta", percentage: "19%", color: "bg-[#facc15]" },
+  { name: "JakLingko", percentage: "12%", color: "bg-[#4a3773]" },
+];
